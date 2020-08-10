e20c40d46435346fa0754a114059e874f5049dea
Hideki Ikemoto <ikemo333@gmail.com>
2020-08-10 13:53:30 +0900

    move .gitconfig to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo 'setup.sh'
+echo 'setup.sh start'
+
+git config --global alias.br branch
+git config --global alias.ch checkout
+git config --global alias.pp 'pull --prune'
+
+echo 'setup.sh end'
