94c986a182790de9419a74e0511d5ba89cd486b4
Hideki Ikemoto <ikemo333@gmail.com>
2020-08-10 16:18:05 +0900

    load bash-completion

--- setup.sh
+++ setup.sh
@@ -6,4 +6,6 @@ git config --global alias.br branch
 git config --global alias.ch checkout
 git config --global alias.pp 'pull --prune'
 
+source /usr/share/bash-completion/completions/git
+
 echo 'setup.sh end'
